7a7652ead0cfb7c8b25e7b2b7fa3eff63353e392
agustinagilmingorance <47931512+agustinagilmingorance@users.noreply.github.com>
2019-03-10 18:52:45 -0300

    Update ocho.js

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,11 +1,19 @@
 function mostrar()
 {
-  var numero; 
-  var letra;
-  var contador=0;
-  var acumulador=0;
-  var letraMin;
-  var numMin;
-  var numNeg;
-  var numPos;
-}
+	var letra; 
+	var numero=parseInt(prompt("ingrese un numero entre -100 y 100"));
+	var contador=0;
+  	var acumulador=0;
+  	var letraMin;
+  	var numMin;
+  	var numNeg;
+  	var numPos;
+
+  	for (contador=0; ;contador++)
+  	{
+  		while(contador=<-100||contador>=100)
+  		{
+  			alert ("ingrese un numero valido ");
+  			numero=parseInt(prompt("reingrese un numero entre -100 y 100"))
+  		}
+  	}
